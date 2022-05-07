518a04f0b91fb0284cbe3dcc5f6ecb9bc7640fbe
jaynai24 <jaynaipalmer1997@icloud.com>
2022-05-07 18:49:35 -0400

    assignments

--- “Basic_HTML_and_JavaScript_1.”/Basic_JavaScript_1.js
+++ “Basic_HTML_and_JavaScript_1.”/Basic_JavaScript_1.js
@@ -1,1 +1,7 @@
 alert("Hello, World! I am an alert Box!")
+function My_First_Function(){
+    var string="Kiss me, I'm Irish!";
+    var result= String.fontcolor("green");
+    document.getElementById("Irish").innerHTML
+    result;
+}
